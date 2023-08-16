068fcb9de8de240025c384994f7281f5c9ca1b45
Kirill Tamarevsky <tamarevsky@hotmail.com>
2023-08-16 22:47:34 +0300

    01

--- hartLogAnalyze/vm/AnalyzeHARTLogVM.cs
+++ hartLogAnalyze/vm/AnalyzeHARTLogVM.cs
@@ -1,15 +1,34 @@
 ﻿
 
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace hartLogAnalyze.vm
 {
     public class AnalyzeHARTLogVM : baseVM
     {
         private mainWindowVM mainWindowVM;
-        private string HARTLog { get; }
+        private List<hartCommand> HartCommands{ get; }
         public AnalyzeHARTLogVM(mainWindowVM  mainWindow, string hartlog)
         {
             mainWindowVM = mainWindow;
-            HARTLog = hartlog;
+            HartCommands = new List<hartCommand>();
+
+            Regex regex = new Regex(@"cmd[\s:]*<(\d*)>[\s]*[\w\s\d\n:\-<>]*data[:\s]*<([\d\w\s]*)>");
+            MatchCollection matches = regex.Matches(hartlog);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    if (match.Success)
+                    {
+                        if ( int.TryParse(match.Groups[1].Value, out int _))
+                        {
+
+                        }
+                    }
+                }
+            }
         }
 
     }
---
+++ hartLogAnalyze/vm/hartCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hartLogAnalyze.vm
+{
+    public class hartCommand : baseVM
+    {
+        public int Number { get; }
+        public byte[] AnswerData { get; }
+
+        public hartCommand(int num, byte[] answData)
+        {
+            Number = num;
+            AnswerData = answData;
+        }
+    }
+}
